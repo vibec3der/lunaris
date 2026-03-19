fdfc93e48341e0a438765dc6888ec31b6535de16
Alva <244687859+4lvaret@users.noreply.github.com>
2026-03-19 20:51:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/scram/scramjet.all.js");
+importScripts("/proxy/scram/scramjet.all.js");
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
